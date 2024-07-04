27659d73346b16b6248f6fa3069af6ecb62d38ce
FelixTran <felixtran2000@gmail.com>
2024-07-04 18:18:50 +0700

    finalize

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,32 +6,39 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { CardProvider } from '@/contexts/CardContext';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
-    if (loaded) {
+   
+  
       SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    
+  }, []);
 
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+
+      <CardProvider>
+        
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="add_card" options={{ headerShown: true, headerBackTitle:"Back", headerTitle:"Add Card" }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      </CardProvider>
+    
     </ThemeProvider>
   );
 }
